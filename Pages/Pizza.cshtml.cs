55a79699561f1e407fa5ba3624833253a78b70c5
betzelem <betzelem@proton.me>
2023-03-06 13:24:50 -0500

    initial commit

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace new_pizza_delivery.Pages;
+
+public class PizzaModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
